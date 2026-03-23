cf1a450eebe9c6301a584105f806c1765cb6b5bb
oscarcederholm1-create <oscar.cederholm1@skola.taby.se>
2026-03-23 09:50:15 +0100

    boxar till hemsidan från 19/3

---
+++ canva.js
@@ -0,0 +1,8 @@
+let canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = 1 * window.innerHeight;
+
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+
+const c = canvas.getContext("2d");
